9beb5e28e8afe285c4a64a555f72e1681488d6b5
anirudhkmitl <62011088@kmitl.ac.th>
2021-10-28 10:59:05 +0700

    [ADDED] Grid that being displayed on the UI

--- MainWindow.py
+++ MainWindow.py
@@ -4,20 +4,25 @@ from Node import Node
 pygame.init()
 
 
+
 ##Colors definition
-WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-YELLOW = (242, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-PURPLE = (161, 3, 252)
-AQUA = (0, 255, 255)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
 
 class MainWindow:
     def __init__(self, width):
-        self.width=width
-        self.height=width
-        self.display=pygame.display.set_mode((self.width,self.height))
+        self.width = width
+        self.height = width
+        self.display = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Path Finder")
 
     def make_grid(self, rows, width):
@@ -30,15 +35,40 @@ class MainWindow:
                 grid[i].append(node)
         return grid
 
-    def run(self):
-        self.running = True
-        clock = pygame.time.Clock()
-        while self.running:
-            dt = clock.tick(40) / 1000.0
-            pygame.display.flip()
+    def draw_grid(self, win, rows, width):
+        gap = width // rows
+        for i in range(rows):
+            pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+            for j in range(rows):
+                pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+
+    def draw(self, win, grid, rows, width):
+        win.fill(WHITE)
+
+        for row in grid:
+            for spot in row:
+                spot.draw(win)
+
+        self.draw_grid(win, rows, width)
+        pygame.display.update()
+
+    def run(self, win, width):
+        ROWS = 50
+        grid = self.make_grid(ROWS, width)
+
+        start = None
+        end = None
+
+        run = True
+        while run:
+            self.draw(win, grid, ROWS, width)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+        pygame.quit()
 
 
-mainWindow = MainWindow(800)
-
-while True:
-    mainWindow.run()
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+mainWindow = MainWindow(WIDTH)
+mainWindow.run(WIN, WIDTH)
