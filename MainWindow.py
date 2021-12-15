8c2f2d45a811e65e0f41b88fe3170283a863ea24
anirudhkmitl <62011088@kmitl.ac.th>
2021-12-15 16:19:34 +0700

    [ADDED] Test Prolog code

--- MainWindow.py
+++ MainWindow.py
@@ -1,5 +1,6 @@
 import pygame
 from Node import Node
+from pyswip import Prolog
 
 pygame.init()
 
@@ -17,6 +18,7 @@ ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
+wall = []
 
 class MainWindow:
     def __init__(self, width):
@@ -62,7 +64,7 @@ class MainWindow:
         return row, col
 
     def run(self, win, width):
-        ROWS = 50
+        ROWS = 8
         grid = self.make_grid(ROWS, width)
 
         start = None
@@ -89,21 +91,40 @@ class MainWindow:
 
                     elif spot != end and spot != start:
                         spot.make_barrier()
+                        prolog.assertz("move_pos(PrevX,PrevY,PrevX,Y) :- Y is PrevY-1. % move one position Up")
+                        prolog.assertz("move_pos(PrevX,PrevY,X,PrevY) :- X is PrevX+1. % move one position right")
+                        prolog.assertz("move_pos(PrevX,PrevY,PrevX,Y) :- Y is PrevY+1. % move one position down")
+                        prolog.assertz("move_pos(PrevX,PrevY,X,PrevY) :- X is PrevX-1. % move one position left")
+
+                        prolog.assertz("notvisited(_, []) :- !.")
+                        prolog.assertz("notvisited(X, [H|T]) :-  X \= H, notvisited(X, T).")
+                        prolog.assertz("findGoalPath(X,Y,X,Y,GoalPath,GoalPath).")
+                        prolog.assertz("findGoalPath(PrevX,PrevY,X,Y,AlreadyVisitedPath,GoalPath) :- barrier(NextX,NextY), move_pos(PrevX,PrevY,NextX,NextY),")
+                        prolog.assertz("notvisited(barrier(NextX,NextY),AlreadyVisitedPath),")
+                        prolog.assertz("findGoalPath(NextX,NextY,X,Y,[barrier(NextX,NextY)|AlreadyVisitedPath],GoalPath).")
+                        prolog.assertz(f"barrier({row},{col})")
+                        #print(list(prolog.query("findGoalPath(0, 0, 2, 0, [], Path)")))
+
 
                 elif pygame.mouse.get_pressed()[2]:  # RIGHT
-                    pos = pygame.mouse.get_pos()
-                    row, col = self.get_clicked_pos(pos, ROWS, width)
-                    spot = grid[row][col]
-                    spot.reset()
-                    if spot == start:
-                        start = None
-                    elif spot == end:
-                        end = None
-
+                    prolog.consult("algorithm.pl")
+                    temp = prolog.query("findGoalPath(0, 0, 2, 0, [], Path)")
+                    print(temp)
+                    # pos = pygame.mouse.get_pos()
+                    # row, col = self.get_clicked_pos(pos, ROWS, width)
+                    # spot = grid[row][col]
+                    # #spot.reset()
+                    # if spot == start:
+                    #     start = None
+                    # elif spot == end:
+                    #     end = None
+        #print(list(prolog.query("findGoalPath(5, 4, 7, 3, [], Path)")))
         pygame.quit()
 
 if __name__ == '__main__':
     WIDTH = 800
+    prolog = Prolog()
+    prolog.consult("algorithm.pl")
     WIN = pygame.display.set_mode((WIDTH, WIDTH))
     mainWindow = MainWindow(WIDTH)
     mainWindow.run(WIN, WIDTH)
